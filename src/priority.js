edeba8296b4890d839c9f5f481b16197ae2e7c7e
Konstanteeen <konstanteeen@yandex.ru>
2020-05-26 18:40:32 +0500

    Убрал учет приоритета скобок при расчете математического выражения, поправил тесты

--- src/priority.js
+++ src/priority.js
@@ -1,12 +1,9 @@
 const getPriority = (operator) => {
   switch (operator) {
-    case '(': return 0;
-    case ')': return 1;
-    case '+': return 2;
-    case '-': return 2;
-    case '*': return 3;
-    case '/': return 3;
-    default: return 4;
+    case '+': return 1;
+    case '-': return 1;
+    case '*': return 2;
+    case '/': return 2;
   }
 };
 
--- tests/checkToken.test.js
+++ tests/checkToken.test.js
@@ -21,7 +21,7 @@ test("check symbol '+' is operator", async () => {
 
 test('check priority for symbol', async () => {
   const result = getPriority('-');
-  expect(result).toBe(2);
+  expect(result).toBe(1);
 });
 
 test('check expression by correct brackets', async () => {
